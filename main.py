79d9183085eecb571d7b123fe6966e28bd0f19e3
vikas972 <40334523+vikas972@users.noreply.github.com>
2023-09-28 23:06:58 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+from pathlib import Path
+
+import appdirs as ad
+
+CACHE_DIR = ".cache"
+
+# Force appdirs to say that the cache dir is .cache
+ad.user_cache_dir = lambda *args: CACHE_DIR
+
+# Create the cache dir if it doesn't exist
+Path(CACHE_DIR).mkdir(exist_ok=True)
+
 import streamlit as st
 import yfinance as yf
 import pandas as pd
